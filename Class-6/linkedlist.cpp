e2b7192db937a116d6f901e82965d79253f999d5
Pranay Bansal <44213415+PranayB200@users.noreply.github.com>
2021-10-23 14:00:38 +0530

    Update linkedlist.cpp

--- Class-6/linkedlist.cpp
+++ Class-6/linkedlist.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+
 class Node {
 
 public:
@@ -72,6 +73,87 @@ Node *insert_at_pos(Node *head, int d, int pos) {
     return head;
 }
 
+Node *delete_begin(Node *head) {
+    if (head == NULL) {
+        return head;
+    }
+    head = head->next;
+
+    return head;
+}
+
+Node *delete_end(Node *head) {
+
+    if (head == NULL or head->next == NULL) {
+        return NULL;
+    }
+
+    Node *temp = head;
+    while (temp->next->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = NULL;
+
+    return head;
+}
+
+Node *delete_at_pos(Node *head, int pos) {
+    if (pos == 0) {
+        return delete_begin(head);
+    }
+
+    Node *temp = head;
+    int count = 0;
+
+    while (temp->next != NULL and count < pos - 1) {
+        temp = temp->next;
+        count++;
+    }
+
+    if (temp->next == NULL) {
+        return head;
+    }
+
+    temp->next = temp->next->next;
+    return head;
+}
+
+Node *reverse_iterative(Node *head) {
+    
+    Node *temp = head;
+    Node *prev = NULL;
+
+    while (temp != NULL) {
+        Node *next_node = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = next_node;
+    }
+
+    return prev;
+}
+
+Node *reverse_recursive(Node *head) {
+
+    if (head == NULL or head->next == NULL) {
+        return head;
+    }
+
+    Node *new_head = reverse_recursive(head->next); // Getting the result of a similar subproblem.
+
+    // Using the result of the subproblem to generate the result of the original problem.
+    Node *temp = new_head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    head->next = NULL;
+    temp->next = head;
+
+    return new_head;
+}
+
 
 int main() {
 
@@ -95,4 +177,25 @@ int main() {
     head = insert_at_pos(head, 3, 5);
 
     print(head); // 1 2 4 5 3
+
+    head = NULL;
+    head = insert_end(head, 1);
+    head = insert_end(head, 2);
+    head = insert_end(head, 3);
+    head = insert_end(head, 4);
+
+    cout << "Before Reversing: ";
+    print(head);
+
+    head = reverse_iterative(head);
+    cout << "After Reversing: ";
+    print(head);
+
+
+    cout << "Before Reverseing: ";
+    print(head);
+
+    head = reverse_recursive(head);
+    cout << "After Reversing: ";
+    print(head);
 }
